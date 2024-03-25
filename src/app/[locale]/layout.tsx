bb0eebdb5dc1e89a8face0c4e619b25e1895073e
Aga Z <agataziemniak1@gmail.com>
2024-03-25 15:43:00 +0100

    chore: added dependencies

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import initTranslations from "@/app/initTranslations";
+import TranslationProvider from "@/app/[locale]/translationProvider";
+
+const i18nNamespaces = ["translation"];
+
+export default async function Layout({ children, params: { locale } }: any) {
+  const { resources } = await initTranslations(locale, i18nNamespaces);
+
+  return (
+    <TranslationProvider
+      locale={locale}
+      resources={resources}
+      namespaces={i18nNamespaces}
+    >
+      {children}
+    </TranslationProvider>
+  );
+}
