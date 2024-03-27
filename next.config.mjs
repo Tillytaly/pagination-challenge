fc98a88780795b646804abf899221a8ce397e495
Aga Z <agataziemniak1@gmail.com>
2024-03-27 11:07:49 +0100

    chore: add different intl version

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: { serverComponentsExternalPackages: ["next-intl"] },
+};
 
 export default withNextIntl(nextConfig);
