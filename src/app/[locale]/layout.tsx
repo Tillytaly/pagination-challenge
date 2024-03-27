fd0e4e5e22d17cfea4dedde2e89a949a3b3baee4
Aga Z <agataziemniak1@gmail.com>
2024-03-27 11:02:00 +0100

    chore: removed static param generation and i18n config

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,15 +2,10 @@ import { notFound } from "next/navigation";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 
 import { locales } from "@/navigation";
-import { i18nConfig } from "@/../../i18nConfig";
 
 import { StyledEngineProvider } from "@mui/material/styles";
 
 import { LayoutProps } from "@/app/types/page";
-
-export function generateStaticParams() {
-  return i18nConfig.locales.map((locale) => ({ locale }));
-}
 
 export default function Layout({ children, params: { locale } }: LayoutProps) {
   if (!locales.includes(locale)) {
